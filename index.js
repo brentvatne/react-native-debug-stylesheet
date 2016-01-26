7007ab98a472fdacc17a941275be01b217730891
Dustin Blackman <dev@dustinblackman.com>
2016-01-26 18:20:04 -0500

    fix dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var StyleSheet = require('react-native').StyleSheet;
 var merge = require('merge');
-var assign = require('Object.assign');
+var assign = require('object-assign');
 
 function randomHexColor() {
   return '#'+('00000'+(Math.random()*16777216<<0).toString(16)).substr(-6);
